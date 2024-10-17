33d06d7b147f13d719f2c89ea6c8a6b6ce6e0df5
Aureliusz Czarnacki <aurelczarnacki@gmail.com>
2024-10-17 16:16:50 +0200

    .

--- PotoDocs/PotoDocs/AppConstants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PotoDocs;
-public static class AppConstants
-{
-    public const string ApiUrl = "https://localhost:44347/";
-    public const string AuthStorageKeyName = "login-flow-auth-key";
-}
