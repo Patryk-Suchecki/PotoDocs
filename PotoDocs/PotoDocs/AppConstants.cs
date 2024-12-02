8af12e838363f5f7d001c3fb73cc7251e68de525
Patryk <patryksuchecki@int.pl>
2024-12-02 22:05:06 +0100

    Refaktoryzacja cd nie pobierać

---
+++ PotoDocs/PotoDocs/AppConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace PotoDocs;
+public static class AppConstants
+{
+    public const string HttpClientName = "poto-docs-http-client";
+    public const string AuthStorageKeyName = "poto-docs-auth-key";
+}
