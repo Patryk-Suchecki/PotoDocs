5da463a8f9a10a75930456f0204d82302113ca8c
Patryk <patryksuchecki@int>
2025-02-14 14:14:18 +0100

    Dodałem dialog potwierdzania operacji

--- PotoDocs.Blazor/Program.cs
+++ PotoDocs.Blazor/Program.cs
@@ -1,5 +1,6 @@
-using BlazorDownloadFile;
+﻿using BlazorDownloadFile;
 using Blazored.LocalStorage;
+using Blazored.Modal;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -11,26 +12,29 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// 🔹 **Dodanie autoryzacji**
 builder.Services.AddAuthorizationCore();
-
 builder.Services.AddScoped<JwtAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
     provider.GetRequiredService<JwtAuthenticationStateProvider>());
 
+// 🔹 **Zewnętrzne usługi Blazored**
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 builder.Services.AddBlazorDownloadFile();
+builder.Services.AddBlazoredModal();
 
-
+// 🔹 **Serwisy aplikacyjne**
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRoleService, RoleService>();
 
+// 🔹 **HttpClient z poprawnym adresem API**
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7157")
 });
 
+// 🔹 **Uruchomienie aplikacji**
 await builder.Build().RunAsync();
-
